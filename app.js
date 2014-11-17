5824641f69297504c9f8d8a8315a164987da3d13
Ashish <itsashis4u@gmail.com>
2014-11-17 13:56:21 +0530

    Tweaked app.js, all other commits

--- app.js
+++ app.js
@@ -17,7 +17,11 @@ var path = require('path');
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, './', 'index.html'));
 })
-
+app.use("/css", express.static(__dirname + '/css'));
+app.use("/js", express.static(__dirname + '/js'));
+app.use("/img", express.static(__dirname + '/img'));
+app.use("/particlejs", express.static(__dirname + '/particleJs'));
+app.use("/bootstrap", express.static(__dirname + '/bootstrap'));
 //used for data communication using GET method
 app.get('/sponsor', function(req, res) {
     res.sendFile(path.join(__dirname, './', 'sponsor.html'));
