697a6b8843a7a9f87c7e9c2477030d87c486edc5
Amulya Kumar Sahoo <amulya349@gmail.com>
2014-12-05 01:39:23 +0530

    add analytics code

--- public/js/particleJs/js/particleapp_2.js
+++ public/js/particleJs/js/particleapp_2.js
@@ -3,6 +3,15 @@
 //require('particles.js');
 /* config dom id */
 //particlesJS('dom-id');
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-57358904-1', 'auto');
+  ga('require', 'displayfeatures');
+  ga('send', 'pageview');
+  
 document.body.style.width= document.body.clientWidth;
 document.body.style.height= document.body.clientHeight;
 document.body.style.background= "#070716";
